bdd2370ee4646cc17280c1e08cea01709cafd689
Ayelen <ayeleennvera@gmail.com>
2026-05-27 08:32:16 -0300

    fix database config for postgres

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,9 @@ Django settings for Shinny Tienda project.
 from decouple import config
 from pathlib import Path
 
+import os
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
@@ -56,10 +59,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
